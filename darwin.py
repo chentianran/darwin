6c622a57569b7962755a731fa01a88c4f69fe574
Nick Ovenhouse <ovenhou3@msu.edu>
2011-10-27 16:29:03 -0400

    translated ruby file into python w/ pyevolve`

--- darwin.py
+++ darwin.py
@@ -1,3 +1,5 @@
+import subprocess
+import re
 
 from pyevolve import G1DList
 from pyevolve import GSimpleGA
@@ -6,25 +8,125 @@ from pyevolve import Initializators
 from pyevolve import Mutators
 from pyevolve import Crossovers
 
-def eval_func(recipe):
-   score = 0
-   # iterate over the chromosome
-   for value in recipe:
-       score += abs(value**3.0)
-   return score
 
+# -------------------------------
+# Variable class
+#
+# ------------------------------
+class Variable:
+	def __init__(self, name, Min, Max, sign):
+		self.name = name
+		self.Min = Min
+		self.Max = Max
+		self.sign = sign
+
+
+	def Eval(self, x):
+		power = self.Min + x*(self.Max-self.Min)
+		return 10**(self.sign*power)
+# ------------------------------------
+
+
+
+
+# -------------------------------
+# Environment class
+#
+# ------------------------------
+class Environment:
+	def __init__(self):
+		self.Vars = list()
+		self.cmds = list()
+
+		self.w_missing  = 200.0
+		self.w_extra    = 200.0
+		self.w_failed   = 100.0
+		self.w_tracking = 1.0
+
+
+	def GetValue(self, match, string):
+		g = re.search(match, string, flags=re.MULTILINE)
+		if (g != None):
+			return float(g.group(1))
+		else:
+			return None
+
+
+	def Fitness(self, recipe):
+		print recipe[0]
+		return 0
+		f = 0.0
+		for c in self.cmds:
+			cmd = list(c)
+			for var in self.Vars:
+				cmd.append( "-f" + str(var.name) + ":" + str(var.Eval( recipe[self.Vars.index(var)] )) )
+
+
+			
+			output = subprocess.check_output( cmd, stderr=subprocess.STDOUT )
+
+			missing  = self.GetValue('^Missing:\s*([0-9]+\.?[0-9]*)', output)
+			extra    = self.GetValue('^Extra:\s*([0-9]+\.?[0-9]*)', output)
+			failed   = self.GetValue('^Failed:\s*([0-9]+\.?[0-9]*)', output)
+			tracking = self.GetValue('^Tracking:\s*([0-9]+\.?[0-9]*)', output)
+
+			if( missing != None ):
+				f = f + missing * self.w_missing
+			if( extra != None ):
+				f = f + extra * self.w_extra
+			if( failed != None ):
+				f = f + failed * self.w_failed
+			if( tracking != None ):
+				print tracking
+				f = f + tracking * self.w_tracking
+
+		return f
+# --------------------------------------------
+
+
+
+# ----------------------------------
+# custom crossover
+# ----------------------------------
 def cross(recipe, **args):
 	return (args["mom"], args["dad"])
 
-genome = G1DList.G1DList(1)
-genome.evaluator.set(eval_func)
-genome.initializator.set(Initializators.G1DListInitializatorReal)
-genome.mutator.set(Mutators.G1DListMutatorRealGaussian);
-genome.setParams(rangemin=-1.0, rangemax=1.0, gauss_mu=0.0, gauss_sigma=0.001)
-genome.crossover.set(cross)
-#genome.crossover.set(Crossovers.G1DListCrossoverUniform)
 
-ga = GSimpleGA.GSimpleGA(genome)
+
+
+
+# ------------------------------------------
+# main driver
+#
+# -----------------------------------------
+
+polysys = ['cassou', 'barry', 'boon', 'heart', 'cyclic5', 'cyclic6', 'cyclic7', 'reimer4', 'reimer5', 'reimer6', 'reimer7']
+options = "--post=jump --post=check-against --no-mp"
+
+env = Environment()
+
+for name in polysys:
+	cmdLine = ["/home/ovenhouse/h3/h3", "-s1", 
+					  "-fanswers:/home/ovenhouse/h3/testcases/answers/" + str(name),
+					  "/home/ovenhouse/h3/testcases/" + str(name) + ".lee"]
+	cmdLine.extend( options.split() )
+	env.cmds.append( cmdLine )
+
+env.Vars.append( Variable("facet-begin",    2.0, 6.0, -1.0) )
+env.Vars.append( Variable("facet-stable",   1.0, 4.0, -1.0) )
+env.Vars.append( Variable("facet-small",    1.0, 4.0, -1.0) )
+env.Vars.append( Variable("facet-negative", 0.7, 4.0, -1.0) )
+
+
+recipe = G1DList.G1DList(4)
+recipe.evaluator.set(env.Fitness)
+recipe.initializator.set(Initializators.G1DListInitializatorReal)
+recipe.mutator.set(Mutators.G1DListMutatorRealGaussian);
+recipe.setParams(rangemin=0.0, rangemax=1.0, gauss_mu=0.0, gauss_sigma=0.001)
+recipe.crossover.set(cross)
+#recipe.crossover.set(Crossovers.G1DListCrossoverUniform)
+
+ga = GSimpleGA.GSimpleGA(recipe)
 ga.pMutation = 0.1
 ga.pCrossover = 0.01
 ga.setGenerations(300)
--- test.py
+++ test.py
@@ -1,14 +1,18 @@
-import os
 import re
 import subprocess
+import os
 
 
-out = subprocess.check_output(['/home/ovenhouse/h3/h3', '/home/ovenhouse/h3/testcases/barry.lee'])
+p = subprocess.check_output(['/home/ovenhouse/h3/h3', 
+	   				  '/home/ovenhouse/h3/testcases/barry.lee'], 
+				      #stdout = subprocess.PIPE, 
+					  stderr = subprocess.STDOUT)
 
-lines = out.splitlines()
+#output = p.communicate()
 
-for line in lines:
-	pos = re.search('^Failed:\s*([0-9]+)', line, flags=re.MULTILINE)
-	if( pos != None ):
-		print pos.group(0)
+
+pos = re.search('^Preprocess:\s*([0-9]+\.?[0-9]*)', p, flags=re.MULTILINE)
+if( pos != None ):
+	print pos.group(1)
+
 	
